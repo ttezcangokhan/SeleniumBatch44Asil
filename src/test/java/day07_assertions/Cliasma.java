833a646c4abe0995b5bcfda0d9282bb00926f477
ttezcangokhan <86927360+ttezcangokhan@users.noreply.github.com>
2022-05-16 23:53:33 +0200

    update160522

---
+++ src/test/java/day07_assertions/Cliasma.java
@@ -0,0 +1,32 @@
+package day07_assertions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Cliasma {
+
+
+static WebDriver driver;
+    @BeforeClass
+    public static void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+
+    }
+    @Test
+    public void url
+
+
+    @AfterClass
+    public static void teardown(){
+        driver.close();
+    }
+}
